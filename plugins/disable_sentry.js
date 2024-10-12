a3875fa7e61ca32249b0dade5f6efa875311811e
bababoi-2 <165707934+bababoi-2@users.noreply.github.com>
2024-10-13 00:54:14 +0200

    Create disable_sentry.js

---
+++ plugins/disable_sentry.js
@@ -0,0 +1,16 @@
+module.exports = {
+    name: "Disable Sentry",
+    description: "Disables the sending of telementry to Sentry.io",
+    context: "preload",
+    scope: "own",
+    func: () => {
+        console.log("Sentry Plugin Loaded");
+        const wait_for_sentry = setInterval(() => {
+            if (window.__SENTRY__) {
+                clearInterval(wait_for_sentry);
+                window.__SENTRY__ = null;
+                console.log("Sentry disabled");
+            }
+        }, 10);
+    }
+}
