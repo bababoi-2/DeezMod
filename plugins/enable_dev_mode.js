1644c82732a884c4a25943c343b09ea7a848bae4
bababoi-2 <165707934+bababoi-2@users.noreply.github.com>
2024-10-13 17:58:58 +0200

    Update and rename enable_dev_tools.js to enable_dev_mode.js

--- plugins/enable_dev_mode.js
+++ plugins/enable_dev_mode.js
@@ -1,13 +1,13 @@
 module.exports = {
     name: "DevTools",
-    description: "Enables DevTools, use ctrl+shift+i to open",
+    description: "Enables dev mode, allowing you to use dev tools with ctrl+shift+i",
     version: "1.0",
     author: "Bababoiiiii",
     context: "main",
     scope: "own",
     func: () => {
-        console.log("Devtools Plugin Loaded");
+        console.log("Dev Mode Plugin Loaded");
         process.env.DZ_DEVTOOLS = "yes";
-        console.log("Devtools enabled");
+        console.log("Dev Mode enabled");
     }
 }
